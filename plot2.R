0774894fbc7063d761495311607f6aeab4117227
Jonathan Ho <jonathan.ho2468@gmail.com>
2020-03-26 23:32:05 -0700

    Edit to cleaning data

--- plot2.R
+++ plot2.R
@@ -4,7 +4,8 @@ library(lubridate)
 
 consumption <- read.table("household_power_consumption.txt",header = TRUE,sep = ";")
 
-newData <- consumption %>% mutate(Date=as.character(dmy(as.character(Date)))) %>% filter(Date%in%c("2007-02-02","2007-02-01")) %>% mutate(Date=paste(Date,as.character(Time),sep=" ")) %>% mutate(Date=ymd_hms(Date))  %>% mutate(Global_active_power=as.numeric(as.character(Global_active_power)))
+newData <- consumption %>% mutate(Date=as.character(dmy(as.character(Date)))) %>% filter(Date%in%c("2007-02-02","2007-02-01")) %>% mutate(Date=paste(Date,as.character(Time),sep=" ")) %>% mutate(Date=ymd_hms(Date))  %>% 
+  mutate(Global_active_power=as.numeric(as.character(Global_active_power))) 
 
 with(newData,plot(Date,Global_active_power,type = "l",ylab = "Global Active Power (kilowatts)",xlab=" "))
 
